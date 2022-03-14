b2edd0a7e5e7f3299a3ae0d813512e4baebe16fb
Sofia Nunes <80426028+sofiarnunes@users.noreply.github.com>
2022-03-14 14:18:09 +0000

    Form validation feature

---
+++ script.js
@@ -0,0 +1,50 @@
+const firstName = document.getElementById('first-name');
+const errorFN = document.getElementById('error-fn');
+const lastName = document.getElementById('last-name');
+const errorLN = document.getElementById('error-ln');
+const email = document.getElementById('email');
+const errorEmail = document.getElementById('error-email');
+const pwd = document.getElementById('password');
+const errorPwd = document.getElementById('error-pwd');
+const submit = document.getElementById('submit');
+
+errorFN.hidden = true;
+errorLN.hidden = true;
+errorEmail.hidden = true;
+errorPwd.hidden = true;
+
+submit.addEventListener('click', function(e) {
+    e.preventDefault();
+    if(!firstName.value.length){
+        errorFN.hidden = false;
+        firstName.classList.add('error-input');
+    } else{
+        errorFN.hidden = true;
+        firstName.classList.remove('error-input');
+    }
+
+    if(!lastName.value.length){
+        errorLN.hidden = false;
+        lastName.classList.add('error-input');
+    } else{
+        errorLN.hidden = true;
+        lastName.classList.remove('error-input');
+    }
+
+    if(!(email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) || !email.value.length){
+        errorEmail.hidden = false;
+        email.classList.add('error-input');
+    } else{
+        errorEmail.hidden = true;
+        email.classList.remove('error-input');
+    }
+
+    if(!pwd.value.length){
+        errorPwd.hidden = false;
+        pwd.classList.add('error-input');
+    } else{
+        errorPwd.hidden = true;
+        pwd.classList.remove('error-input');
+    }
+})
+
